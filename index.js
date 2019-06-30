989f3318ffd286ca552ce9d70973f2705c82464a
Allan Cheng <allanhfcheng@gmail.com>
2019-06-30 00:07:33 -0700

    read article close modal

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var dogCount = 0;
 
 'use strict';
-var YTclips =[];
-var Newsclips =[];
+var YTclips = [];
+var Newsclips = [];
 
-const apiKey ="352d6669b5f446188152fd93ee7ccf52";
+const apiKey = "352d6669b5f446188152fd93ee7ccf52";
 
 const searchURL = 'https://newsapi.org/v2/everything';
 
 
-function displayVideoModal(){
+function displayVideoModal() {
   $('.container').on('click', '.description', function (event) {
     $('#videoModal').css('display', 'block');
-    
+
     console.log("index is " + $(this).attr('index'));
     var index = $(this).attr('index');
-  
+
     $('.videoModal-content').html(
-    
-  `
+
+      `
   <button class="close">&times;</button>
   <p> ${YTclips[index].title}</p>
   <hr>
@@ -27,41 +27,34 @@ function displayVideoModal(){
   <div class= "buttonContainer">
   <a href="https://www.youtube.com/embed/${YTclips[index].videoId}" class ="videoUrlButton" index="i" data-lity>Go to Video</a>
  </div>
- 
   `
-      );
-  
-  
+    );
   });
-  
 }
 
-function closeVideoModal(){
+function closeVideoModal() {
   $('.container').on('click', '.close', function (event) {
     $('#videoModal').css("display", "none");
-    $('.videoModal-content').html( `<button class="close">&times;</button>`);
-  
+    $('.videoModal-content').html(`<button class="close">&times;</button>`);
   });
-  
 }
 
-function watchVideo(){
+function watchVideo() {
   $('.container').on('click', '.videoUrlButton', function (event) {
     $('#videoModal').css("display", "none");
-
   });
 }
 
-function displayNewsModal(){
+function displayNewsModal() {
   $('.container').on('click', '.newsDescription', function (event) {
     $('#newsModal').css('display', 'block');
-    
+
     console.log("index is " + $(this).attr('index'));
     var index = $(this).attr('index');
-  
+
     $('.newsModal-content').html(
-    
-  `
+
+      `
   <button class="close">&times;</button>
   <p> ${Newsclips[index].title}</p>
   <hr>
@@ -70,28 +63,21 @@ function displayNewsModal(){
   <div class= "buttonContainer">
   <a href=" ${Newsclips[index].url}" class ="readArticleButton" target="_blank">Read Article</a>
  </div>
- 
   `
-      );
-  
-  
+    );
   });
-  
 }
 
-function closeNewsModal(){
+function closeNewsModal() {
   $('.container').on('click', '.close', function (event) {
     $('#newsModal').css("display", "none");
-    $('.newsModal-content').html( `<button class="close">&times;</button>`);
-  
+    $('.newsModal-content').html(`<button class="close">&times;</button>`);
   });
-  
 }
 
-function readArticle(){
-  $('.container').on('click', '.newsArticleButton', function (event) {
+function readArticle() {
+  $('.container').on('click', '.readArticleButton', function (event) {
     $('#newsModal').css("display", "none");
-
   });
 }
 function formatQueryParams(params) {
@@ -100,19 +86,18 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function getNews(query, maxResults=10) {
+function getNews(query, maxResults = 10) {
   const params = {
     q: query,
     language: "en",
   };
   const queryString = formatQueryParams(params)
   const url = searchURL + '?' + queryString;
-
   console.log(url);
-
   const options = {
     headers: new Headers({
-      "X-Api-Key": apiKey})
+      "X-Api-Key": apiKey
+    })
   };
 
   fetch(url, options)
@@ -129,50 +114,37 @@ function getNews(query, maxResults=10) {
 }
 
 function getYTImages(query) {
-    console.log(query);
-  
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=7&q=${query}&key=AIzaSyDOGebeDBNkAfaUNohpEAqrb2J6NXFRzhs`;
-    console.log(url);
+  console.log(query);
+
+  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=7&q=${query}&key=AIzaSyDOGebeDBNkAfaUNohpEAqrb2J6NXFRzhs`;
+  console.log(url);
   fetch(url)
     .then(response => response.json())
-    .then(responseJson => 
-    {
-     // STORE = {ytapi: responseJson};
-      // ytapi = responseJson;
-     //console.log("ytapi is " + ytapi.items[0]);
-     console.log(responseJson)
+    .then(responseJson => {
+      console.log(responseJson)
       displayResults(responseJson)
-     // displayModal()
-    })
-    .catch(error =>{
 
+    })
+    .catch(error => {
       console.log(error.message);
       alert('Something went wrong. Try again later.');
-    } 
+    }
     )
-    
+
 }
 
-function formatDate(date){
-  
-  var d=new Date(date);
-  var returnDate=`${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`
-return returnDate;
+function formatDate(date) {
+
+  var d = new Date(date);
+  var returnDate = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`
+  return returnDate;
 }
 
 function displayResults(responseJson) {
- /* console.log(responseJson);
-  console.log(responseJson.items[1]);
-  console.log(responseJson.length);*/
+
   $('.results-img').html("");
-  let link="";
-  //replace the existing image with the new one
-  
-for(let i =0; i< responseJson.items.length;i++)
-  {
-   // link += `<img src="${responseJson.message[i]}" class="results-img">`;
-  
-  //display the results section
+  let link = "";
+  for (let i = 0; i < responseJson.items.length; i++) {
     var youtubeOject = {
       videoId: responseJson.items[i].id.videoId,
       url: responseJson.items[i].snippet.thumbnails.default.url,
@@ -182,9 +154,9 @@ for(let i =0; i< responseJson.items.length;i++)
       description: responseJson.items[i].snippet.description
     }
 
-    YTclips[i]=youtubeOject;
-  $('.results-img').append(
-    `<li>
+    YTclips[i] = youtubeOject;
+    $('.results-img').append(
+      `<li>
     
     <div class="image"><a href="https://www.youtube.com/embed/${responseJson.items[i].id.videoId} " data-lity><img src="${responseJson.items[i].snippet.thumbnails.high.url}" class="youtube-image"  title="youtube image" alt="youtube image"></a>
     </div>
@@ -197,36 +169,20 @@ for(let i =0; i< responseJson.items.length;i++)
     </div>
     </li>
     `);
-   //`<li><iframe width="420" height="345" src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}">
-   //</iframe></li>`);
-
-
-
-
-  
   }
 
-  console.log("store is "+ YTclips[3].title);
-  /*$('.results-img').html(
-    image
-   );*/
-     $('.results-news').html(
-    
-   );
+  console.log("store is " + YTclips[3].title);
+
+  $('.results-news').html(
+  );
   $('.results').removeClass('hidden');
 }
 
 function displayNewsResults(responseJson, maxResults) {
-  // if there are previous results, remove them
+
   console.log(responseJson);
   $('.results-news').empty();
-  // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
-
+  for (let i = 0; i < responseJson.articles.length & i < maxResults; i++) {
     var newsOject = {
       title: responseJson.articles[i].title,
       source: responseJson.articles[i].source.name,
@@ -235,7 +191,7 @@ function displayNewsResults(responseJson, maxResults) {
       url: responseJson.articles[i].url
     }
 
-    Newsclips[i]=newsOject;
+    Newsclips[i] = newsOject;
 
     $('.results-news').append(
       `<li>
@@ -248,23 +204,20 @@ function displayNewsResults(responseJson, maxResults) {
       <p class="source">${responseJson.articles[i].source.name} By ${responseJson.articles[i].author}</p>
       <p class ="real-description"> ${responseJson.articles[i].description}</p>
       <button type="button" class= "newsDescription" index="${i}">Read Description</button>
-      
       </div>
       </li>`
-    )};
-  //display the results section  
+    )
+  };
+
   $('.results').removeClass('hidden');
 };
 
-
-
 function watchForm() {
-    var query;
+  var query;
   $('form').submit(event => {
-    query=$('.query-number').val();
+    query = $('.query-number').val();
     console.log(query);
     event.preventDefault();
-   // $('#myModal').css('display', 'block')
     getYTImages(query);
     getNews(query, 7);
   });
@@ -277,7 +230,7 @@ function watchForm() {
   readArticle();
 }
 
-$(function() {
+$(function () {
   console.log('App loaded! Waiting for submit!');
   $(".query-number").attr("value", 'cat');
   watchForm();
